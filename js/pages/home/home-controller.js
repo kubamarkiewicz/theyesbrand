e243dbbb5f7c1fdd67a621c90e4f03782fbd16c7
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2018-04-18 17:58:43 +0200

    home done

--- js/pages/home/home-controller.js
+++ js/pages/home/home-controller.js
@@ -1,3 +1,7 @@
 app.controller('HomeController', function($scope, $rootScope, $http, $routeParams, config) {  
 
+	$('section#home-banners .container').appear(function() {
+     	$(this).addClass('appeared');
+    });
+
 });
